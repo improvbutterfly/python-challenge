f79a1101813ad8094778fbebabccc09bfeae256f
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2020-08-29 20:52:18 -0700

    Calculated total months, corrected csv filepath

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,21 +4,35 @@ import os
 # import Module for reading CSV files
 import csv
 
-budget_path = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_path = os.path.join('.', 'Resources', 'budget_data.csv')
 
 #Open CSV file
 with open(budget_path) as budget_file:
 	# read the CSV file
     budget_reader = csv.reader(budget_file, delimiter=',')
 
+    # Set initial variables to 0
+    total_months = 0
+    total_profit_loss = 0
 
-    # Calculate months included in the dataset
+    for row in budget_reader:
+	    # Calculate months included in the dataset
+	    if row[0] != "Date":
+	    	total_months = total_months + 1
+
+	    # Calculate the net total amount of "Profit/Losses" over the entire period
 
 
-    # Calculate the net total amount of "Profit/Losses" over the entire period
+	    # Calculate the greatest increase in profits (date and amount) over the entire period
 
 
-    # Calculate the greatest increase in profits (date and amount) over the entire period
+    	# Calculate the greatest decrease in losses (date and amount) over the entire period
 
 
-    # Calculate the greatest decrease in losses (date and amount) over the entire period
+    # Print data to screen
+
+    # Test total_months data
+    print(f"Total months: {total_months}")
+
+    # Print data to text file
+
