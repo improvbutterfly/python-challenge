8c9cbe46a3bbb576f2a03fa4cdb638db774c41ed
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2020-08-31 11:00:37 -0700

    Beginning PyPoll code, outputs just total votes at the moment

---
+++ PyPoll/main.py
@@ -0,0 +1,40 @@
+# Program to read CSV with "voter ID," "county" and "candidate" data, analyse results, and output to screen and text file
+# Authored by Dominica Corless
+
+# import the os module to join the file paths across operating systems
+import os
+
+# import Module for reading CSV files
+import csv
+
+election_path = os.path.join('.', 'Resources', 'election_data.csv')
+
+# Path for output text file
+#election_results = os.path.join('.', 'analysis', 'election_results.txt')
+
+# Create a dictionary to store the votes for each candidate
+candidates = {}
+
+#Open CSV file
+with open(election_path) as election_file:
+	# read the CSV file
+	election_reader = csv.reader(election_file, delimiter=',')
+
+	#Initialize variables
+	total_votes = 0
+
+	for row in election_reader:
+		voter_ID = row[0]
+		county = row[1]
+		candidate = row[2]
+
+		if voter_ID != "Voter ID":
+			total_votes = total_votes + 1
+
+# Print election results
+print("Election Results")
+print("----------------------------")
+
+# Print total votes
+print(f"Total Votes: {total_votes}")
+print("----------------------------")
